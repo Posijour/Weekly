a7ac247abeeb0a96fc4c95d6b87a502551188cb2
Posijour <168458508+Posijour@users.noreply.github.com>
2026-03-12 08:25:25 +0200

    Create config.py

---
+++ weekly_stats/config.py
@@ -0,0 +1,14 @@
+SUPABASE_URL = "https://qcusrlmueapuqbjwuwvh.supabase.co"
+SUPABASE_KEY = "sb_publishable_VsMaZGz98nm5lSQZJ-g-kQ_bUOfSO_r"
+
+TABLE_NAME = "logs"
+PAGE_SIZE = 1000
+REQUEST_TIMEOUT = 30
+
+DERIBIT_EVENT_NAMES = {"deribit_vbi_snapshot", "deribit_vbi_snasphot"}
+
+EXPECTED_TWEET_COUNT = 5
+TWEET_MAX_LEN = 260
+MAX_NA_PER_TWEET = 6
+
+TWITTER_POST_URL = "https://api.twitter.com/2/tweets"
